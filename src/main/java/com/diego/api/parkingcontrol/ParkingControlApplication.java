510214e4a5476fcf20bb1cc57beac429a77ec63a
diegocardosodev <diegocardososacramento@gmail.com>
2022-02-21 14:52:27 -0300

    init

---
+++ src/main/java/com/diego/api/parkingcontrol/ParkingControlApplication.java
@@ -0,0 +1,13 @@
+package com.diego.api.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/api/parkingcontrol/ParkingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.api.parkingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
