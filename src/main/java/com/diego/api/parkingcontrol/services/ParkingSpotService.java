04b9c7107258306e86fc83c989509ba6ae0ba520
diegocardosodev <diegocardososacramento@gmail.com>
2022-02-22 09:07:42 -0300

    methods in serevice implements

--- src/main/java/com/diego/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/diego/api/parkingcontrol/services/ParkingSpotService.java
@@ -3,20 +3,23 @@ package com.diego.api.parkingcontrol.services;
 
 import com.diego.api.parkingcontrol.models.ParkingSpotModel;
 import com.diego.api.parkingcontrol.repositories.ParkingSpotRepository;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.awt.print.Pageable;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-@Data
+@AllArgsConstructor
 @Service
 public class ParkingSpotService {
 
     private final ParkingSpotRepository parkingSpotRepository;
 
-    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
-        this.parkingSpotRepository = parkingSpotRepository;
-    }
+
 
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkSpotModel) {
@@ -24,15 +27,40 @@ public class ParkingSpotService {
 
     }
 
-    public boolean existsLincencePlateCar(String lincesePlateCar) {
-        return parkingSpotRepository.existsLincencePlateCar(lincesePlateCar);
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
     }
 
-    public boolean existsParkNumber(String parkingSpotNumber) {
-        return parkingSpotRepository.existsParkNumber(parkingSpotNumber);
+    public Void delete(UUID id){
+        parkingSpotRepository.deleteById(id);
+        return null;
     }
 
-    public boolean existsByAPtoBlock(String apartament, String block) {
-        return parkingSpotRepository.existsByAPtoBlock(apartament, block);
+
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
     }
+
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+    public Page<ParkingSpotModel> findAll(Pageable pageable) {
+        return parkingSpotRepository.findAll((org.springframework.data.domain.Pageable) pageable);
+    }
+
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+
 }
