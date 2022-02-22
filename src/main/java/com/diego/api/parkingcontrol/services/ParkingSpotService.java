af49091a037a8e7c250ae6f3d1b8f372d01bc5b9
diegocardosodev <diegocardososacramento@gmail.com>
2022-02-22 09:17:26 -0300

    methods REFATORY

--- src/main/java/com/diego/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/diego/api/parkingcontrol/services/ParkingSpotService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.awt.print.Pageable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -17,47 +18,48 @@ import java.util.UUID;
 @Service
 public class ParkingSpotService {
 
+    /*INJEÇÃO DO REPOSITORY*/
     private final ParkingSpotRepository parkingSpotRepository;
 
 
 
+    /*METODO PARA SALVAR A VAGA NO SISTEMA*/
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel parkSpotModel) {
         return parkingSpotRepository.save(parkSpotModel);
 
     }
 
-    public List<ParkingSpotModel> findAll() {
-        return parkingSpotRepository.findAll();
-    }
-
-    public Void delete(UUID id){
-        parkingSpotRepository.deleteById(id);
-        return null;
-    }
-
-
-    public boolean existsByLicensePlateCar(String licensePlateCar) {
-        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
-    }
-
-    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
-        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
-    }
-
-    public boolean existsByApartmentAndBlock(String apartment, String block) {
-        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
-    }
-
+    /*METODO PARA LISTAR TODOS*/
     public Page<ParkingSpotModel> findAll(Pageable pageable) {
         return parkingSpotRepository.findAll((org.springframework.data.domain.Pageable) pageable);
     }
 
+
+
+    /*METODO PARA VERIFICAR A LICENÇA*/
+    public boolean existsByLicensePlateCar(String licensePlateCar) {
+        return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    /*METODO PARA VERIFICAR O NUMERO DA VAGA, SE JÁ EXITE*/
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+
+    /*METODO PARA VERIFICAR O NUMERO DO BLOCO E APARTAMENTO SE JA EXISTE*/
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+    }
+
+
+    /*METODO PARA DELETAR VAGA*/
     @Transactional
     public void delete(ParkingSpotModel parkingSpotModel) {
         parkingSpotRepository.delete(parkingSpotModel);
     }
 
+    /*METODO PARA PROCURAR POR ID*/
     public Optional<ParkingSpotModel> findById(UUID id) {
         return parkingSpotRepository.findById(id);
     }
